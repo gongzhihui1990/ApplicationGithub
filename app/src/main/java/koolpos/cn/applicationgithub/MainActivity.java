4db15038238c05c5a6bfaf99bc7d5f8b344f15d0
Heisenberg <gongzhihui1990@gmail.com>
2017-05-12 13:38:38 +0800

    版本2

--- app/src/main/java/koolpos/cn/applicationgithub/MainActivity.java
+++ app/src/main/java/koolpos/cn/applicationgithub/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //版本2
+        //版本3
     }
 }
